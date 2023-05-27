c6aebddc3de48cb9f8282cd93968f1c1b6587aa7
Jen Nguyen <jennifernguyen0000@gmail.com>
2023-05-27 15:44:02 -0400

    Functional background change feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
+import variables from './variables.scss'
 import React, { useState } from 'react'
+import styled, { keyframes } from 'styled-components'
 import Pomodoro from './components/Pomodoro.js'
 import Quote from './components/Quote.js'
 import Todo from './components/Todo.js'
 import Hydrate from './components/Hydrate.js'
 import Sound from './components/Sound.js'
+
+
 
 const Nav = ({name, clickEvent}) => {
   return (
@@ -20,6 +24,74 @@ const Nav = ({name, clickEvent}) => {
     )
   }
 
+  const Background = ({open, set}) => {
+    const [bg, setBg] = useState([
+      {
+        name: 'btn-1',
+        id: 1,
+        color: variables.bg_option1,
+      },
+      {
+        name: 'btn-2',
+        id: 2,
+        color: variables.bg_option2,
+      },
+      {
+        name: 'btn-3',
+        id: 3,
+        color: variables.bg_option3,
+      },
+      {
+        name: 'btn-4',
+        id: 4,
+        color: variables.bg_option4,
+      },
+      {
+        id: 5,
+        color: variables.bg_option5,
+      },
+      {
+        name: 'btn-6',
+        id: 6,
+        color: variables.bg_option6,
+      },
+    ])
+
+
+    const pickColor = (color) => {
+      set(color)
+    }
+
+    
+    if(open === true) {
+      return(
+      <div className='btn-cont'>
+     {bg.map((curr_bg => <button key={curr_bg.id} style={{background: curr_bg.color}} className='btn-option' onClick={() => pickColor(curr_bg.color)}></button>))}
+      </div>
+    )
+    }
+  }
+
+
+const gradient = keyframes`
+0% {
+  background-position: 0% 50%;
+}
+50% {
+  background-position: 100% 50%;
+}
+100% {
+  background-position: 0% 50%;
+}
+`;
+
+const StyledMain = styled.main`
+  background:${props => props.$inputColor};
+  height: 100vh;
+  background-size: 400% 400%;
+  animation: ${gradient} 12s ease infinite;
+  position: relative;
+`;
 const App = () => {
 
   const [navBar, setNavBar] = useState([
@@ -50,8 +122,9 @@ const App = () => {
     }, 
   ])
 
+  const [openBG, setOpenBG] = useState(false)
+  const [userBG, setUserBG] = useState(variables.bg_option1)
 
-  // Will be cleaning this up later...
   const handleContainer = (curr_container) => {
     const newArray = navBar.map(nav => {
       if(nav.id === curr_container) {
@@ -61,27 +134,29 @@ const App = () => {
     })
     setNavBar(newArray)
   }
-
+  const openBackground = () => {setOpenBG(!openBG)}
+ 
   return (
     <>
-    <main>
-        <Logo/>
-    <nav>
-      <ul className="nav-bar">
-        {navBar.map((option) => {
-          return(
-            <Nav name={option.name}
-                  key={option.id}
-                  clickEvent={() => handleContainer(option.id)}/>)})}
-        <li className='nav-list'><button className='bg-button'></button></li>
-      </ul>
-    </nav>
+    <StyledMain $inputColor={userBG}>
+      <Logo/>
+      <nav>
+      <Background open={openBG} set={setUserBG}/>
+        <ul className="nav-bar">
+          {navBar.map((option) => {
+            return(
+              <Nav name={option.name}
+                key={option.id}
+                clickEvent={() => handleContainer(option.id)}/>)})}
+          <li className='nav-list'><button className='bg-button' style={{background: userBG}} onClick={openBackground}></button></li>
+        </ul>
+      </nav>
     <Todo container={navBar[1].clicked}/>
     <Quote container={navBar[2].clicked}/>
     <Pomodoro container={navBar[0].clicked}/>
     <Hydrate container={navBar[3].clicked}/>
     <Sound container={navBar[4].clicked}/>
-</main>
+</StyledMain>
     </>
       
     
